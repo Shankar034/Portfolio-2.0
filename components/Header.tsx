0a8177a591fb8f85a73db75c9a280e9ea574fe22
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-01-25 10:30:45 +0545

    framer-motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
-import { ReactDOM } from 'react';
+import { motion } from "framer-motion"
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky p-5 top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <div className='flex  flex-row items-center'>
+        <div 
+              initial={{
+                x: -500,
+                opacity:0,
+                scale:0.5
+              }}
+              animate={{
+                x:0,
+                opacity:1,
+                scale:1
+              }}
+        className='flex  flex-row items-center'>
             {/* Social Icons */}
             
             <SocialIcon url="https://youtube.com/@shankarbhandari9914" fgColor='gray' bgColor='transparent'/>
@@ -15,7 +26,19 @@ export default function Header({}: Props) {
             <SocialIcon url="https://youtube.com/@shankarbhandari9914" fgColor='gray' bgColor='transparent'/>
             <SocialIcon url="https://youtube.com/@shankarbhandari9914" fgColor='gray' bgColor='transparent'/>
         </div>
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <div 
+        initial ={{
+          x:500,
+          opacity:0,
+          scale:0.5,
+        }}
+        animate={{
+          x:0,
+          opacity:1,
+          scale:1,
+        }}
+        transition={{duration:1}}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'>
             {/* Avatar */}
             <SocialIcon 
             className='cursor-pointer'
