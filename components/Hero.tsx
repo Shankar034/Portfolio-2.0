e2f96d572373a9442a72ed4384b5360a3f1b7e15
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-01-30 13:20:48 +0545

    text styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,8 +17,21 @@ export default function Hero({}: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden '>
         <BackgroundCircles />
-        <span>{text}</span>
-        <Cursor cursorColor="orange"/>
+       
+        <img  
+        className='relative rounded-full h-32 w-32 mx-auto'
+        // src="shankar.png/"
+        src="https://avatars.githubusercontent.com/u/50977378?v=4" 
+        alt="" />
+        <div>
+          <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
+            Software Engineer
+          </h2>
+          <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+            <span className='mr-3'>{text}</span>
+            <Cursor cursorColor="orange"/>
+          </h1>
+        </div>
     </div>
   )
 }
