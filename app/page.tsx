efdb940c563d86be684f485ac76dde70edb2352c
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-02-03 10:10:33 +0545

    Building the work experience section

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 import Header from '../components/Header'
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-
+import WorkExperience from '@/components/WorkExperience'
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
@@ -25,6 +25,9 @@ export default function Home() {
         </section>
 
         {/* Experience */}
+        <section id='experience' >
+          <WorkExperience/>
+        </section>
 
         {/* Skills */}
 
