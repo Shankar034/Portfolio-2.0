be7f7ab4418c2095f39385d980c150b326c3c203
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-02-13 11:13:22 +0545

    Building the contact me section

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { PhoneIcon,MapPinIcon, EnvelopeIcon } from '@heroicons/react/24/solid'
 type Props = {}
 
 function ContactMe({}: Props) {
@@ -13,6 +13,29 @@ function ContactMe({}: Props) {
              I have got just you need.{""}
              <span className='decoration-[#f7ab01]/50 underline'>Lets Talk</span>
             </h4>
+            <div className='soace-y-10'>
+              <div className='flex items-center space-x-5 justify-center'>
+                <PhoneIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse'/>
+                <p className='text-2xl'>+123456789</p>
+              </div> 
+              <div className='flex items-center space-x-5 justify-center'>
+                <EnvelopeIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse'/>
+                <p className='text-2xl'>shankar.bhandari@sagarmatha.edu.np</p>
+              </div> 
+              <div className='flex items-center space-x-5 justify-center'>
+                <MapPinIcon className='text-[#f7ab0a] h-7 w-7 animate-pulse'/>
+                <p className='text-2xl'>123 Developer Lane</p>
+              </div> 
+            </div>
+            <form className='flex flex-col space-y-2 w-fit mx-auto'>
+              <div className='flex space-x-2'>
+                <input placeholder='Next' className='contactInput' type="text" />
+                <input placeholder='Email' className='contactInput'  type="text" />
+              </div>
+              <input placeholder='Subject' className='contactInput'  type="text" />
+              <textarea placeholder='Message' className='contactInput' />
+              <button className='bg-[#f7ab0a] py-5 px-10 rounded-md text-black font-bold text-lg'>Submit</button>
+            </form>
         </div>
     </div>
   )
