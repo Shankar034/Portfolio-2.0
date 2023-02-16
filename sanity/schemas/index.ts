396f1d2e4960e4694c969f825f426879f1439001
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-02-16 12:49:21 +0545

    Adding in Sanity Schema

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,6 +1,6 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
+import pageInfo from './pageInfo'
 import author from './author'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [pageInfo, author, category, blockContent]
