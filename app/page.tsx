0cd929c4476e21f250c87674d1d34e7a41c020e7
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-02-22 16:25:31 +0545

    Implementing incremental static regeneration

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,28 @@ import Skills from '@/components/Skills'
 import Projects from '@/components/Projects'
 import ContactMe from '@/components/ContactMe'
 import Link from "next/link"
+import { GetStaticProps} from 'next'
+import { Experience, PageInfo, Project, Skill, Social } from '@/typings'
+import { fetchPageInfo } from '@/utils/fetchPageInfo';
+import { fetchProjects } from '@/utils/fetchProjects'
+import { fetchExperiences } from '@/utils/fetchExperiences'
+import { fetchSkills } from '@/utils/fetchSkills'
+import { fetchSocials } from '@/utils/fetchSocials'
+
+
+
+type Props = {
+  pageInfo: PageInfo;
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Social[]
+}
+
+
+
 const inter = Inter({ subsets: ['latin'] })
-export default function Home() {
+const Home = ({pageInfo, experiences, projects, socials, skills}: Props)=>{
   return (
     
     <div className='bg-[#242424] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/80'>
@@ -65,4 +85,27 @@ export default function Home() {
   
    
   );
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props > = async () =>{
+
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchPageInfo();
+  const skills: Skill[] = await fetchPageInfo();
+  const projects: Project[] = await fetchPageInfo();
+  const socials: Social = await fetchPageInfo();
+  return{
+    props:{
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+
+    },
+    revalidate: 10,
+  };
+
 };
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,6 +1,6 @@
 import { PageInfo } from "@/typings";
 
-export const fetchProjects = async() =>{
+export const fetchPageInfo = async() =>{
     const res = await fetch (`${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`);
 
     const data = await res.json()
