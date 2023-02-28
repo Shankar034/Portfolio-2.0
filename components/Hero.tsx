9ebf636398dd6de121a2585ca30d4e1d8911b24e
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-02-28 22:07:58 +0545

    Adding sanity data in Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero({pageInfo}: Props) {
         alt="" />
         <div className='z-20'>
           <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>
-            Software Engineer
+           { pageInfo?.role}
           </h2>
           <h1 className='text-5xl lg:text-6xl font-semibold scroll-px-10 '>
             <span className='mr-3'>{text}</span>
