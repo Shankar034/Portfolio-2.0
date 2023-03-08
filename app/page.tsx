aea0783d0b4d602573ada4840778099333b20550
Shankar Bhandari <50977378+Shankar034@users.noreply.github.com>
2023-03-08 13:28:39 +0545

    Deploying the versel

--- app/page.tsx
+++ app/page.tsx
@@ -63,7 +63,7 @@ const Home = ({pageInfo, experiences, projects, socials, skills}: Props)=>{
 
         {/* Projects */}
         <section id="projects" className='snap-start'>
-          <Projects/>
+          <Projects projects={projects}/>
         </section>
 
         {/* Contact Me */}
